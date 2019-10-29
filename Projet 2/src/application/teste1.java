78108d8adc2ab581c0000244fab84302462dbf3f
Leger Philemon <P.Leger@cgodin.qc.ca>
2019-10-29 10:37:08 -0400

    29-10-Phil: test

--- Projet 2/src/application/teste1.java
+++ Projet 2/src/application/teste1.java
@@ -2,6 +2,10 @@ package application;
 
 public class teste1 {
 
+	public void yeet() {
+			System.out.println("yeet");
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("tete2");
 		System.out.println("shrekv2");
@@ -9,6 +13,9 @@ public class teste1 {
 		
 		System.out.println("autre test");
 		System.out.println("testligne");
+		
+		
+		
 		
 	}
 }
