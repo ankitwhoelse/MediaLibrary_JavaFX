67085abd79a57961c0adac49bc94ba1615b2b9a4
Patel Ankit <1577750@cgodin.qc.ca>
2019-11-05 10:59:16 -0500

    05-11-AP: interface 90% done

--- Projet 2/src/vue/Interface.java
+++ Projet 2/src/vue/Interface.java
@@ -8,21 +8,26 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
@@ -35,8 +40,11 @@ import modele.Document;
 public class Interface extends Application{
 
 	BorderPane root, root2;
-	Button btnConn, btnBiblio;
-	TextField txtPrenom, txtNom, txtTel;
+	Button btnConn, btnBiblio, btnCons, btnSearch;
+	TextField txtPrenom, txtNom, txtTel, txtRecherche;
+	Text txt1, txt2, txt3, txt4;
+	CheckBox cbConn;
+	RadioButton rbAuteur, rbMotsCles;
 	Scene scene, scene2;
 	Stage stage2;
 
@@ -47,10 +55,12 @@ public class Interface extends Application{
 //			
 //					< INTERFACE DE CONNECTION >
 			root = new BorderPane();
+			GestionClick gc = new GestionClick();
 			
 			VBox vBox = new VBox();
 			vBox.setPadding(new Insets(10));
 			VBox.setMargin(vBox, new Insets(10));
+			vBox.setSpacing(10);
 			
 			GridPane gPane = new GridPane();
 			GridPane.setHalignment(gPane, HPos.LEFT);
@@ -58,25 +68,25 @@ public class Interface extends Application{
 			gPane.setHgap(5);
 			gPane.setVgap(5);
 			
-			Text txt1 = new Text("Identifiez vous");
+			txt1 = new Text("Identifiez vous");
 			gPane.add(txt1, 0, 0);
 			txt1.setFont(Font.font("Arial", FontWeight.BOLD, FontPosture.ITALIC, 20));
 			
-			Text txt2 = new Text("Votre nom:");
+			txt2 = new Text("Votre nom:");
 			gPane.add(txt2, 0, 1);
 			txtNom = new TextField("");
 			txtNom.setPrefWidth(100);
 			txtNom.setMinWidth(100);
 			gPane.add(txtNom, 1, 1);
 			
-			Text txt3 = new Text("Votre prénom:");
+			txt3 = new Text("Votre prénom:");
 			gPane.add(txt3, 0, 2);
 			txtPrenom = new TextField("");
 			txtPrenom.setPrefWidth(100);
 			txtPrenom.setMinWidth(100);
 			gPane.add(txtPrenom, 1, 2);
 			
-			Text txt4 = new Text("Votre numéro de téléphone:");
+			txt4 = new Text("Votre numéro de téléphone:");
 			gPane.add(txt4, 0, 3);
 			txtTel = new TextField("");
 			txtTel.setPrefWidth(100);
@@ -84,24 +94,35 @@ public class Interface extends Application{
 			gPane.add(txtTel, 1, 3);
 			
 			
-			GestionClick gc = new GestionClick();
-			btnConn = new Button();
-			btnConn.setText("Connexion");
-			btnConn.setOnAction(gc);
-			btnConn.setPrefWidth(200);
-			btnConn.setMinWidth(200);
+			btnCons = new Button();
+			btnCons.setText("Connexion");
+			btnCons.setOnAction(gc);
+			btnCons.setPrefWidth(255);
+			btnCons.setMinWidth(255);
+			btnCons.setPrefHeight(33);
+			btnCons.setMaxHeight(33);
 			
 			btnBiblio = new Button();
 			btnBiblio.setText("Consulter la médiathèque");
 			btnBiblio.setOnAction(gc);
-			btnBiblio.setPrefHeight(200);
-			btnBiblio.setMinWidth(200);
+			btnBiblio.setPrefHeight(255);
+			btnBiblio.setMinWidth(255);
+			btnBiblio.setPrefHeight(33);
+			btnBiblio.setMaxHeight(33);
+
+			cbConn = new CheckBox();
+			cbConn.setText("Préposé/Admin");
+			cbConn.setOnAction(gc);
 			
-			vBox.getChildren().addAll(gPane, btnConn, btnBiblio);
+			
+			vBox.getChildren().addAll(gPane, btnCons, btnBiblio, cbConn);
+			vBox.setAlignment(Pos.CENTER);
 			root.setCenter(vBox);
+			BorderPane.setAlignment(root, Pos.BOTTOM_CENTER);
 			
-			scene = new Scene(root,300,300);
-			primaryStage.setTitle("Connexion");
+			scene = new Scene(root,270,250);
+			primaryStage.setResizable(false);
+			primaryStage.setTitle("Acceuil");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 //					< / INTERFACE DE CONNECTION >
@@ -122,6 +143,30 @@ public class Interface extends Application{
 			VBox vBox2 = new VBox();
 			vBox2.setSpacing(10);
 			
+			HBox hBoxSearch = new HBox();
+			hBoxSearch.setAlignment(Pos.CENTER_LEFT);
+			hBoxSearch.setSpacing(10);
+			HBox.setMargin(hBoxSearch, new Insets(5));
+			Text txtSearch = new Text("Recherche par: ");
+			VBox.setMargin(hBoxSearch, new Insets(5));
+			
+			ToggleGroup tgSearch = new ToggleGroup();
+			rbAuteur = new RadioButton();
+			rbAuteur.setText("auteur");
+			rbMotsCles = new RadioButton();
+			rbMotsCles.setText("mots clés");
+			tgSearch.getToggles().addAll(rbAuteur, rbMotsCles);
+			
+			txtRecherche = new TextField("");
+			txtRecherche.setPrefWidth(100);
+			txtRecherche.setMinWidth(100);
+			
+			btnSearch = new Button();
+			btnSearch.setText("Rechercher");
+			btnSearch.setOnAction(gc);
+			
+			hBoxSearch.getChildren().addAll(txtSearch, rbAuteur, rbMotsCles, txtRecherche, btnSearch);
+			
 			
 //				onglet documents
 			Tab tabDoc = new Tab();
@@ -130,11 +175,13 @@ public class Interface extends Application{
 //			tabDoc.setGraphic(new ImageView(new Image("icon-collection.png")));
 			
 			final TableView<Document> tableDoc = new TableView<Document>();
-			TableColumn<Document, String> colonneNum1 = new TableColumn<Document, String> ("Numero");
+			TableColumn<Document, String> colonneNum1 = new TableColumn<Document, String> ("Numéro");
 			TableColumn<Document, String> colonneTitre1 = new TableColumn<Document, String> ("Titre");
 			TableColumn<Document, LocalDate> colonneParution1 = new TableColumn<Document, LocalDate> ("Date de parution");
 			TableColumn<Document, String> colonneDispo1 = new TableColumn<Document, String> ("Disponible");
 			tableDoc.getColumns().addAll(colonneNum1, colonneTitre1, colonneParution1, colonneDispo1);
+//			tableDoc.setItems();	// get list documentd
+			
 			
 			colonneNum1.setPrefWidth(120);
 			colonneNum1.setMaxWidth(120);
@@ -151,15 +198,15 @@ public class Interface extends Application{
 			Tab tabDvd = new Tab();
 			tabDvd.setClosable(false);
 			tabDvd.setText("DVD");
-	//		tabDoc.setGraphic(new ImageView(new Image("icon-dvd.png")));
+//			tabDoc.setGraphic(new ImageView(new Image("icon-dvd.png")));
 			
 			final TableView<Document> tableDvd = new TableView<Document>();
-			TableColumn<Document, String> colonneNum2 = new TableColumn<Document, String> ("Numero");
+			TableColumn<Document, String> colonneNum2 = new TableColumn<Document, String> ("Numéro");
 			TableColumn<Document, String> colonneTitre2 = new TableColumn<Document, String> ("Titre");
 			TableColumn<Document, LocalDate> colonneParution2 = new TableColumn<Document, LocalDate> ("Date de parution");
 			TableColumn<Document, String> colonneDispo2 = new TableColumn<Document, String> ("Disponible");
-			TableColumn<Document, Integer> colonneDisk = new TableColumn<Document, Integer> ("Nombre de disque");
-			TableColumn<Document, String> colonneRealis = new TableColumn<Document, String> ("Realisateur");
+			TableColumn<Document, Integer> colonneDisk = new TableColumn<Document, Integer> ("Nombre de disques");
+			TableColumn<Document, String> colonneRealis = new TableColumn<Document, String> ("Réalisateur");
 			tableDvd.getColumns().addAll(colonneNum2, colonneTitre2, colonneParution2, colonneDispo2, colonneDisk, colonneRealis);
 			
 			colonneNum2.setPrefWidth(120);
@@ -184,11 +231,11 @@ public class Interface extends Application{
 	//		tabDoc.setGraphic(new ImageView(new Image("icon-livre.png")));
 			
 			final TableView<Document> tableLivre = new TableView<Document>();
-			TableColumn<Document, String> colonneNum3 = new TableColumn<Document, String> ("Numero");
+			TableColumn<Document, String> colonneNum3 = new TableColumn<Document, String> ("Numéro");
 			TableColumn<Document, String> colonneTitre3 = new TableColumn<Document, String> ("Titre");
 			TableColumn<Document, LocalDate> colonneParution3 = new TableColumn<Document, LocalDate> ("Date de parution");
 			TableColumn<Document, String> colonneDispo3 = new TableColumn<Document, String> ("Disponible");
-			TableColumn<Document, Integer> colonneMCle = new TableColumn<Document, Integer> ("Mots cles");
+			TableColumn<Document, Integer> colonneMCle = new TableColumn<Document, Integer> ("Mots clés");
 			TableColumn<Document, String> colonneAuteur = new TableColumn<Document, String> ("Auteur");
 			tableLivre.getColumns().addAll(colonneNum3, colonneTitre3, colonneParution3, colonneDispo3, colonneMCle, colonneAuteur);
 			
@@ -210,16 +257,16 @@ public class Interface extends Application{
 	//			onglet periodique
 			Tab tabPerio = new Tab();
 			tabPerio.setClosable(false);
-			tabPerio.setText("Periodique");
+			tabPerio.setText("Périodique");
 	//		tabDoc.setGraphic(new ImageView(new Image("icon-livre.png")));
 			
 			final TableView<Document> tablePerio = new TableView<Document>();
-			TableColumn<Document, String> colonneNum4 = new TableColumn<Document, String> ("Numero");
+			TableColumn<Document, String> colonneNum4 = new TableColumn<Document, String> ("Numéro");
 			TableColumn<Document, String> colonneTitre4 = new TableColumn<Document, String> ("Titre");
 			TableColumn<Document, LocalDate> colonneParution4 = new TableColumn<Document, LocalDate> ("Date de parution");
 			TableColumn<Document, String> colonneDispo4 = new TableColumn<Document, String> ("Disponible");
-			TableColumn<Document, Integer> colonneVol = new TableColumn<Document, Integer> ("Numero de volume");
-			TableColumn<Document, Integer> colonnePerio = new TableColumn<Document, Integer> ("Numero de periodique");
+			TableColumn<Document, Integer> colonneVol = new TableColumn<Document, Integer> ("Numéro de volume");
+			TableColumn<Document, Integer> colonnePerio = new TableColumn<Document, Integer> ("Numéro de périodique");
 			tablePerio.getColumns().addAll(colonneNum4, colonneTitre4, colonneParution4, colonneDispo4, colonneVol, colonnePerio);
 			
 			colonneNum4.setPrefWidth(120);
@@ -232,19 +279,43 @@ public class Interface extends Application{
 			colonneDispo4.setMaxWidth(120);
 			colonneVol.setPrefWidth(120);
 			colonneVol.setMaxWidth(120);
-			colonnePerio.setPrefWidth(120);
-			colonnePerio.setMaxWidth(120);
+			colonnePerio.setPrefWidth(130);
+			colonnePerio.setMaxWidth(130);
 			tabPerio.setContent(tablePerio);
 			
 			
+//			onglet personnel
+			Tab tabCompt = new Tab();
+			tabCompt.setClosable(false);
+			tabCompt.setText("Compte");
 			
-			tabPane.getTabs().addAll(tabDoc, tabDvd, tabLivre, tabPerio);
+			VBox vBoxUtil = new VBox();
+		
+			Text txtUtil = new Text();
+			txtUtil.setText("Utilisateur: ");
+			VBox.setMargin(txtUtil, new Insets(10));
+			
+			vBoxUtil.getChildren().addAll(txtUtil);
+			tabCompt.setContent(vBoxUtil);
+			
+			
+			tabPane.getTabs().addAll(tabDoc, tabDvd, tabLivre, tabPerio, tabCompt);
+			
+			
+			HBox hBoxBtn = new HBox();
 			
 			Button btnQuit = new Button();
-			btnQuit.setText("Fermer le catalogue");
-			btnQuit.setOnAction(e -> stage2.close());
+			btnQuit.setText("Quitter");
+			btnQuit.setOnAction(e -> {stage2.close(); primaryStage.close();});
+			HBox.setMargin(btnQuit, new Insets(10));
+			
+			Button btnFermer = new Button();
+			btnFermer.setText("Fermer le catalogue");
+			btnFermer.setOnAction(e -> stage2.close());
+			HBox.setMargin(btnFermer, new Insets(10));
 					
-			vBox2.getChildren().addAll(tabPane, btnQuit);
+			hBoxBtn.getChildren().addAll(btnQuit, btnFermer);
+			vBox2.getChildren().addAll(hBoxSearch, tabPane, hBoxBtn);
 			root2.setCenter(vBox2);
 			
 			
@@ -259,7 +330,7 @@ public class Interface extends Application{
 	public class GestionClick implements EventHandler<ActionEvent> {
 		
 		public void handle(ActionEvent e) {
-			if (e.getSource() == btnConn) {
+			if (e.getSource() == btnCons) {
 				//	CAS OÙ LES CASES SONT VIDES
 				if ( txtNom.getLength()==0 && txtPrenom.getLength()==0 && txtTel.getLength()==0) {
 					Optional<ButtonType> retour = afficherBoiteInfo(0);
@@ -273,18 +344,43 @@ public class Interface extends Application{
 					stage2.showAndWait();
 					
 					
-				} else {}
-				
+				} else {
+					
+				}
 				
 			}
 			
+			
 			if (e.getSource() == btnBiblio) {
+				stage2.showAndWait();
+			}
+			
+			
+			if (cbConn.isSelected()) {
+				txt4.setVisible(false);
+				txtTel.setVisible(false);
+				txt1.setText("Connectez vous");
+				txt2.setText("ID:");
+				txt3.setText("Mot de passe:");
 				
+				
+			} else if (!cbConn.isSelected()) {
+				txt4.setVisible(true);
+				txtTel.setVisible(true);
+				txt1.setText("Identifiez vous");
+				txt2.setText("Votre nom:");
+				txt3.setText("Votre prénom:");
+				
+				
+			}
+	
+			if (e.getSource() == btnSearch) {
+				if (txtRecherche.getLength()==0) {
+					Optional<ButtonType> retour = afficherBoiteInfo(11);
+				}
 			}
 			
 		}
-	
-
 	}
 	
 	public Optional<ButtonType> afficherBoiteInfo(int cas) {
@@ -309,10 +405,15 @@ public class Interface extends Application{
 			alert=new Alert(AlertType.INFORMATION);
 			alert.setTitle("Connection effectuée");
 			alert.setHeaderText("");
-			alert.setContentText("Vous êtes connectés à votre compte.");
+			alert.setContentText("Vous êtes connectés à votre dossier.");
 			break;
 			
-		
+		case 11:
+			alert=new Alert(AlertType.ERROR);
+			alert.setTitle("Recherche");
+			alert.setHeaderText("");
+			alert.setContentText("Veuillez assurer que la case de recherche soit remplie.");
+			break;
 		
 		default:
 			break;
