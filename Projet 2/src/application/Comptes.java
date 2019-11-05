509d17113cde2ae3ce2cbc952e2acf021cf6097c
Leger Philemon <P.Leger@cgodin.qc.ca>
2019-11-05 10:38:37 -0500

    05-11-Phil: ajout des classes adhérant, preéposé, administrateur et prêt

--- Projet 2/src/application/Comptes.java
+++ Projet 2/src/application/Comptes.java
@@ -6,6 +6,14 @@ public class Comptes {
 		
 	public Comptes(String id) {
 		// TODO Auto-generated constructor stub
+		this.setId(id);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
 		this.id = id;
 	}
 
---
+++ Projet 2/src/application/Prepose.java
@@ -0,0 +1,10 @@
+package application;
+
+public class Prepose extends Comptes {
+
+	public Prepose(String id) {
+		super(id);
+		// TODO Auto-generated constructor stub
+	}
+
+}
